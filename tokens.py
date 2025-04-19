80034c3d1bb7f76dbbb71f4e4abb2b6cb5895d40
Astral <md.mahatabmahimn@gmail.com>
2025-04-19 20:27:59 +1200

    Created a Lexer

---
+++ tokens.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from dataclasses import dataclass
+
+class TokenType(Enum):
+    NUMBER          = 0
+    PLUS            = 1
+    MINUS           = 2
+    MULTIPLICATION  = 3
+    DIVISION        = 4
+    L_PAREN         = 5
+    R_PAREN         = 6
+
+@dataclass
+class Token:
+    type: TokenType
+    value: any = None
+
+    # If we print out a token this will print it out in a nice format
+    # So this prints out the token value if it has a value
+    def __repr__(self):
+        return self.type.name + (f": {self.value}" if self.value != None else "")
+ 
